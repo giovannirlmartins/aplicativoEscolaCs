ce371afc2c4c80598eaeb78e27d6dbf5d4628190
giovannirlmartins <giovannirlmartins@gmail.com>
2026-02-25 16:29:31 -0300

    primeiro commit

---
+++ Professor.cs
@@ -0,0 +1,69 @@
+using System.ComponentModel.Design;
+
+namespace escola;
+public class Professor
+{
+    public string? nome;
+    public string? dataNascimento;
+    protected string? bairro;
+    protected string? estado;
+    protected string? pais;
+    private string? login;
+    private string? senha;
+
+
+    public Professor CadastrarProfessor(Professor professorCadastro)
+    {
+        Console.WriteLine("Digite o nome do Professor: ");
+        professorCadastro.nome = Console.ReadLine();
+        Console.WriteLine("Digite a data de nascimento: ");
+        professorCadastro.dataNascimento = Console.ReadLine() ?? "";
+        Console.WriteLine("Digite o bairro: ");
+        professorCadastro.bairro = Console.ReadLine() ?? "";
+        Console.WriteLine("Digite o estado: ");
+        professorCadastro.estado= Console.ReadLine() ?? "";
+        Console.WriteLine("Digite o País: ");
+        professorCadastro.pais = Console.ReadLine() ?? "";
+        Console.WriteLine("Digite o login: ");
+        professorCadastro.login = Console.ReadLine() ?? "";
+        Console.WriteLine("Digite a senha: ");
+        professorCadastro.senha = Console.ReadLine() ?? ""; 
+        return professorCadastro;
+
+    }
+
+    public void Menu(){
+        
+    }
+    public bool Login(List <Professor> listaProfessores)
+    {
+        int tentativas = 3;
+
+        do{
+            Console.WriteLine("========TELA DE LOGIN========");
+            Console.WriteLine("Digite seu Login: ");
+            string? loginAtual = Console.ReadLine();
+            Console.WriteLine("Digite sua Senha: ");
+            string? senhaAtual = Console.ReadLine();
+
+            foreach (var professor in listaProfessores)
+            {
+                if(loginAtual==professor.login && senhaAtual == professor.senha)
+                {
+                    Console.WriteLine("Login Realizado com Sucesso"); 
+                    return true;             
+                }
+            }
+            tentativas = tentativas-1;
+
+            Console.WriteLine("Login/Senha incorretos");
+            Console.WriteLine($"Você ainda tem {tentativas} tentativas");
+
+        }while(tentativas!=0);
+
+        return false;
+        
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace escola; //school;
+
+public class Program
+{
+    static void Main()
+    {
+        // tipo nomeDaVariavel;
+        float nota1, nota2, nota3, media;
+        string opcao;        
+
+        List <Professor> professores = new List<Professor>();
+
+        Professor professorAtual = new Professor();
+
+        professorAtual = professorAtual.CadastrarProfessor(professorAtual);
+        professores.Add(professorAtual);
+
+        bool estaLogado = professorAtual.Login(professores);
+
+        if(estaLogado == true){
+            do{
+            
+            Console.WriteLine("Bem-Vindo Professor(a) " + professorAtual.nome);
+            
+            Console.WriteLine("Digite a nota 1 do aluno: ");
+            nota1 = float.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine("Digite a nota 2 do aluno: ");
+            nota2 = float.Parse(Console.ReadLine() ?? "");
+            Console.WriteLine("Digite a nota 3 do aluno: ");
+            nota3 = float.Parse(Console.ReadLine() ?? "");
+
+            media = (nota1+nota2+nota3)/3;
+
+            Console.WriteLine("A média do aluno é: " + media.ToString("F2"));
+            //Console.WriteLine($"A média do aluno é: {media}");
+            //Se a media for maior ou igual a 7 = APROVADO, se a media for menor que 7 
+            // e maior ou igual a 3 = RECUPERAÇÃO, menor que 3 = REPROVADO
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Você está APROVADO!");
+            }
+
+            else if (media < 7 && media>=3)
+            {
+                Console.WriteLine("Você está em RECUPERAÇÃO.");
+            }
+            else
+            {
+                Console.WriteLine("Você está REPROVADO!");
+            }
+
+            Console.WriteLine("Deseja calcular a média de outro aluno?");
+            Console.WriteLine("Digite 's' para sim e 'n' para não");
+            opcao = Console.ReadLine() ?? "";
+            }while(opcao=="s" || opcao=="S");
+        }
+        
+        Console.WriteLine("Não pode acessar a área do aluno. Faça seu login novamente!");
+    }
+}
